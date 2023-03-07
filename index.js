9505e48dee2f9c7fe52d4ee04a748930b315bf75
planOfRecordXYZ <126621657+planOfRecordXYZ@users.noreply.github.com>
2023-03-07 18:33:26 -0500

    added drop down

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 
 // Plan of Record Button
 
-var pan = document.getElementsByClassName("panel")[0];
 var acc = document.getElementsByClassName("accordion")[0];
+var pan = document.getElementsByClassName("panel");
+var i;
 
 acc.addEventListener("click", function() {
-   if (pan.style.maxHeight) {
-     pan.style.maxHeight = null;
+for (i = 0; i < 4; i++) {
+    
+   if (pan[i].style.maxHeight) {
+     pan[i].style.maxHeight = null;
    } else {
-     pan.style.maxHeight = pan.scrollHeight + "px";
+     pan[i].style.maxHeight = pan[i].scrollHeight + "px";
 }
+ }
 });
