11f69794c6606e3c831c48e51ddea0d1ba51d608
planOfRecordXYZ <126621657+planOfRecordXYZ@users.noreply.github.com>
2023-03-07 20:41:16 -0500

    Put P5JS in a DIV

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
-function setup () {
-    createCanvas(windowWidth, 1000);
+
+function sketch_testDiv(p) {
+    p.setup = function() {
+    p.createCanvas(p.windowWidth - (p.windowWidth*.05), 1000);
     
 }
 
-function draw(){
-    background(255, mouseX*.1, mouseY*.1);
-    noStroke();
-    fill(255);
-    ellipse(mouseX, mouseY, 40, 40);
+p.draw = function(){
+    p.background(255, p.mouseX*.1, p.mouseY*.1);
+    p.noStroke();
+    p.fill(255);
+    p.ellipse(p.mouseX, p.mouseY, 40, 40);
 }
+
+}
+
+new p5(sketch_testDiv, "testDiv")
