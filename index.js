faa979f2535abb0657f15b2de1a6aa20644c8fbd
planOfRecordXYZ <126621657+planOfRecordXYZ@users.noreply.github.com>
2023-03-14 21:43:12 -0400

    Added upper title text to button Lines Added space between titles and text reduced size of footer

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 
 // Plan of Record Button
 
-var acc = document.getElementsByClassName("accordion")[0];
+var acc = document.getElementsByClassName("accordion");
 var pan = document.getElementsByClassName("panel");
 var i;
 
-acc.addEventListener("click", function() {
 for (i = 0; i < 4; i++) {
+acc[i].addEventListener("click", function() {
+  for (i = 0; i < 4; i++) {
     
    if (pan[i].style.maxHeight) {
      pan[i].style.maxHeight = null;
    } else {
      pan[i].style.maxHeight = pan[i].scrollHeight + "px";
 }
- }
+}
 });
+}
